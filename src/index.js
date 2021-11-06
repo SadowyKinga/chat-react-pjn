c44598c3b7a2cf2d03fedee868d764064409bc1e
Kinga Sadowy <ksadowy@betasi.pl>
2021-11-06 23:43:26 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,41 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import "antd/dist/antd.css";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './firebase'
-import App from "./app/common/components/app/app";
-
-import Reducer from './_reducers';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import promiseMiddleware from 'redux-promise';
-import ReduxThunk from 'redux-thunk';
-import { BrowserRouter } from "react-router-dom";
-
-import * as serviceWorker from "./serviceWorker";
-const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
-
-const url = new URLSearchParams(window.location.search);
-document.documentElement.style.setProperty('--primary', url.get("primary"))
-document.documentElement.style.setProperty('--user', url.get("secondary"))
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider
-    store={createStoreWithMiddleware(
-      Reducer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__()
-    )}
-  >
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-  ,
-  document.getElementById("root"),
-
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
